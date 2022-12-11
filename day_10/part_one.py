e924c3427ba24d2494cc1740c329f0a158894bd0
Elena <elena@tinybird.co>
2022-12-11 22:54:29 +0100

    Refactor day 10

--- day_10/part_one.py
+++ day_10/part_one.py
@@ -1,6 +1,4 @@
-def main():
-    file = open('./day_10/input_sample', 'r')
-    lines = file.readlines()
+def get_cycles(lines):
     CYCLES = [20, 60, 100, 140, 180, 220]
     x = 1
     next_cycle = 0
@@ -10,15 +8,22 @@ def main():
     for line in lines:
         i = line.strip().split()
         c, value = (2, int(i[1])) if i[0] == 'addx' else (1, 0)
+
+        for i in range(next_cycle, next_cycle + c):
+            if i not in cycles:
+                cycles[i] =  cycles[i - 1] if i > 2 else 1
+            if i in CYCLES:
+                total += i * cycles[i]
+
         next_cycle = next_cycle + c
         x = x + value
         cycles[next_cycle] = x
+    return total
 
-    for c in range(1, next_cycle):
-        if c not in cycles:
-            cycles[c] = cycles[c - 1] if c > 2 else 1
-        if c in CYCLES:
-            total += c * cycles[c]
+def main():
+    file = open('./day_10/input', 'r')
+    lines = file.readlines()
+    total = get_cycles(lines)
 
     print(f'** Total: {total}')
 
--- day_10/part_two.py
+++ day_10/part_two.py
@@ -1,34 +1,42 @@
-def main():
-    file = open('./day_10/input', 'r')
-    lines = file.readlines()
+def get_cycles(lines):
     x = 1
     next_cycle = 0
     cycles = {}
-    LINE_LENGTH = 40
 
     for line in lines:
         i = line.strip().split()
         c, value = (2, int(i[1])) if i[0] == 'addx' else (1, 0)
+
+        for i in range(next_cycle, next_cycle + c):
+            if i not in cycles:
+                cycles[i] =  cycles[i - 1] if i > 2 else 1
+
         next_cycle = next_cycle + c
         x = x + value
         cycles[next_cycle] = x
+    return next_cycle, cycles
 
-    for c in range(1, next_cycle):
-        if c not in cycles:
-            cycles[c] = cycles[c - 1] if c > 2 else 1
-
+def print_lines(total_cycles, cycles):
+    LINE_LENGTH = 40
     x = 1
     current_cycle = 1
+    sprite = [x - 1, x, x + 1]
 
-    for i in range(0, int(next_cycle / LINE_LENGTH)):
+    for _ in range(0, int(total_cycles / LINE_LENGTH)):
         line = ''
-        sprite = [x - 1, x, x + 1]
         for j in range(0, LINE_LENGTH):
             line += '#' if j in sprite else '.'
             x = cycles[current_cycle]
             sprite = [x - 1, x, x + 1]
             current_cycle += 1
         print(line)
+
+def main():
+    file = open('./day_10/input', 'r')
+    lines = file.readlines()
+
+    total_cycles, cycles = get_cycles(lines)
+    print_lines(total_cycles, cycles)
 
 if __name__ == "__main__":
     main()
